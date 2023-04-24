c9be7d5864fa7e2a0e4e1f50384e22eafc1b4d3d
ParasMarios <paraske.marios@gmail.com>
2023-04-24 19:16:54 +0300

    Add validation dependency and change the id type.

--- student-management/src/main/java/com/mparaske/studentmanagement/model/Student.java
+++ student-management/src/main/java/com/mparaske/studentmanagement/model/Student.java
@@ -1,7 +1,6 @@
 package com.mparaske.studentmanagement.model;
 
 import lombok.Data;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 @Data
@@ -9,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Student {
 
     @Id
-    private ObjectId Id;
+    private String Id;
     private String firstName;
     private String lastName;
     private String email;
--- student-management/src/main/java/com/mparaske/studentmanagement/model/Thesis.java
+++ student-management/src/main/java/com/mparaske/studentmanagement/model/Thesis.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Thesis {
 
     @Id
-    private ObjectId id;
+    private String id;
     private String title;
     private String description;
     private Integer maxNumberOfStudents;
