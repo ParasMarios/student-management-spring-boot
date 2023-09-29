3883e249d3b93d6dfcb5a74a1bf87a3edb66db75
ParasMarios <paraske.marios@gmail.com>
2023-09-29 21:11:58 +0300

    Now allowedOrigins is read from the application.properties file.

--- student-management/src/main/java/com/mparaske/studentmanagement/config/CorsConfig.java
+++ student-management/src/main/java/com/mparaske/studentmanagement/config/CorsConfig.java
@@ -1,20 +1,36 @@
 package com.mparaske.studentmanagement.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("#{'${allowed.origins}'.split(',')}")
+    private final List<String> rawOrigins;
+
+    public CorsConfig(List<String> rawOrigins) {
+        this.rawOrigins = rawOrigins;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(getOrigin())
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
                 .allowCredentials(true);
+    }
+
+    public String[] getOrigin() {
+        int size = rawOrigins.size();
+        String[] originArray = new String[size];
+        return rawOrigins.toArray(originArray);
     }
 }
 
